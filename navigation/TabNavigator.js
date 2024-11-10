a9758e9bb9ade3891a57a5d9ed74517f3780505f
EstelleJiani <jianiguoestelle@gmail.com>
2024-11-09 22:46:17 -0800

    Fix common stacks related navigation bugs

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,44 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+import SearchScreen from '../screens/search/SearchScreen';
+import ShoppingListScreen from '../screens/list/ShoppingListScreen';
+import AccountScreen from '../screens/account/AccountScreen';
+
+export default function TabNavigator() {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name='Search'
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name='search' color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name='List'
+        component={ShoppingListScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name='list-alt' color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name='Account'
+        component={AccountScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name='account-circle' color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
