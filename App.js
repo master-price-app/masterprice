3168c3ad95acf88406b2f14ec28422b1b71107b7
annanfu <fu.anna@northeastern.edu>
2024-11-10 17:05:01 -0800

    Remove the mart data initialization code from App.js

--- App.js
+++ App.js
@@ -2,14 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
 import AppNavigator from "./navigation/AppNavigator";
-// temporary import to initialize Mart data
-import { initializeMartData } from './services/martService';
-
-// temporary initialization of Mart data
-console.log("Starting mart data initialization...");
-initializeMartData()
-  .then(() => console.log("Mart data initialized successfully"))
-  .catch(console.error);
 
 export default function App() {
   return (
