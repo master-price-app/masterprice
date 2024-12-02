c804fcce8288c110418f9978ae3e361053a2bd68
EstelleJiani <jianiguoestelle@gmail.com>
2024-12-02 12:00:52 -0800

    Reorder hours in TimePickerModal to start with 12 for natural time flow

--- components/TimePickerModal.js
+++ components/TimePickerModal.js
@@ -10,7 +10,7 @@ import {
 import { MaterialIcons } from "@expo/vector-icons";
 
 const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const HOURS = Array.from({ length: 12 }, (_, i) => i + 1);
+const HOURS = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 const MINUTES = [0, 15, 30, 45];
 const PERIODS = ['AM', 'PM'];
 
